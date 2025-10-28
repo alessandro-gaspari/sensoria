f6ab505286b2601a712a97446f36c1687c123d87
Alessandro Gaspari <alessandro@MacBook-Air-di-Alessandro.local>
2025-10-28 12:48:41 +0100

    fixes

--- app.py
+++ app.py
@@ -146,4 +146,4 @@ def handle_data_request(data):
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 10000))
-    socketio.run(app, host='0.0.0.0', port=port, debug=False)
+    socketio.run(app, host='0.0.0.0', port=port, debug=False, allow_unsafe_werkzeug=True)
