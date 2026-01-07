be642218539501b1baff49cb87185dab905bf2b5
Alessandro Gaspari <alessandro@Mac.wind3.hub>
2026-01-07 17:13:18 +0100

    visual fixes

--- static/dashboard.js
+++ static/dashboard.js
@@ -1230,7 +1230,7 @@ function initSockCharts() {
 
   const makeOpts = (container, title) => ({
     width: container.offsetWidth,
-    height: 130,
+    height: 150,
     scales: {
       x: { time: false }, // secondi dall'inizio attività
       y: { auto: true }
